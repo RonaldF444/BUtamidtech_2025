6b4e813ac69ae300098f43d449b0e484cfacdc88
Ben Bickel <gdbenb@gmail.com>
2025-03-01 21:13:00 -0500

    modified app to remove routes as main.tsx has it. I was causing an interferance

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from "react-router-dom";
+import HomeScreen from "./pages/HomeScreen";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
 import Dashboard from "./pages/Dashboard";
@@ -6,7 +7,8 @@ import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<LoginPage />} />
+      <Route path="/" element={<HomeScreen />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
